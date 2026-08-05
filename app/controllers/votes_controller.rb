7edf8ce1fcfd2264a9ec6eb98598c1444cc5c60f
Rudzainy Rahman <rudzainy@gmail.com>
2026-08-05 12:28:59 +0800

    Phase 4: HTML votes controller — cast_vote + turbo_stream replace of _vote_bars

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,13 @@
+class VotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @hujah = Hujah.friendly.find(params[:slug])
+    @hujah.cast_vote(by: current_user, choice: params[:vote])
+
+    respond_to do |format|
+      format.turbo_stream # create.turbo_stream.erb — replaces the _vote_bars widget in place
+      format.html { redirect_to hujah_path(@hujah.slug) }
+    end
+  end
+end
