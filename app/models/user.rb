589336536cf143beff5d062ebdf7cb6555e3265c
Rudzainy Rahman <rudzainy@gmail.com>
2026-09-06 14:05:54 +0800

    Wire omniauth-mydigital-id-ruby into Devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable,
-    :omniauthable, omniauth_providers: [:google_oauth2]
+    :omniauthable, omniauth_providers: [:google_oauth2, :my_digital_id]
 
   has_one_attached :avatar
 
